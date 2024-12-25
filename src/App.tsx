f34456ecfc4566f906cbc6929d6c7a850e4b1721
win9xCIH <cihwinx@gmail.com>
2024-12-25 15:25:59 +0300

    bgOpacity

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
   const pointsToAdd = 12;
   const energyToReduce = 12;
 
+  const [bgOpacity, setBgOpacity] = useState(0.86);
 
   useEffect(() => {
     localStorage.setItem('points', points.toString());
